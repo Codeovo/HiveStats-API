0526161dc7ad16dab644bf536d909be153bf3529
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-23 17:02:54 -0400

    Add basic endpoint

---
+++ Sources/App/Networking/Hive/HiveEndpoint.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+enum Hive {
+    private let apiVersion = "v1"
+    
+    // MARK: - Server
+    case playerCount
+    case uniqueCount
+    case globalAchievements
+    
+    // MARK: - Player
+    case player(name: String)
+    case playerGame(name: String, gameType: String)
+}
+
+extension Hive: Endpoint {
+    var base: String {
+        return "https://api.hivemc.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .playerCount: return "/\(apiVersion)/server/playercount"
+        case .uniqueCount: return "/\(apiVersion)/server/uniquecount"
+        case .globalAchievements: return "/\(apiVersion)/server/achievements"
+            
+        case .player(let name): return "/\(apiVersion)/player/\(name)"
+        case .playerGame(let name, let game): return "/\(apiVersion)/player/\(name)/\(game)"
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        default: return []
+        }
+    }
+}
